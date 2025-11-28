402a081621f61ca41a01512e5d00a38d66792311
pablo PC Casa <pablo yuri>
2025-11-28 11:38:54 -0300

    Autenticador adicionado

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
 import Todos from './components/Todos.vue'
 </script>
 
 <template>
   <main>
-    <Todos />
+    <authenticator>
+      <template v-slot="{ singOut }">
+        <Todos />
+        <button @click="singOut">Sing Out</button>
+      </template>
+    </authenticator>>
   </main>
 </template>
 
